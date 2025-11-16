fdf2b651ae25a7148d7113884cd7037237fb8974
Allen Pei <32986987+PeiAllen@users.noreply.github.com>
2025-11-16 00:35:43 -0500

    Fix Ponder Time and New Model

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from .utils import chess_manager, GameContext
 from chess import Move
 import random
 import time
-import chess 
+import chess
 from torch import Tensor
 from chess import Board, Move
 
@@ -20,15 +20,17 @@ from .utils import chess_manager, GameContext
 import torch
 
 # init logic
-model_path = "./src/utils/checkpoint_15000_20251115_220732.pth"
+model_path = "./src/utils/checkpoint_2400_20251116_051414.pth"
 model = LeelaCNN(10, 128)
-model.load_state_dict(torch.load(model_path, weights_only=True, map_location=torch.device('cpu')))
+model.load_state_dict(
+    torch.load(model_path, weights_only=True, map_location=torch.device("cpu"))
+)
 
 # wrap the model
 wrapped_model = ModelWrapper(model)
 
-# create mcts with the model 
-mcts = MCTS(wrapped_model)
+# create mcts with the model
+mcts = MCTS(wrapped_model, c_puct=3.0)
 
 
 def random_move(ctx: GameContext) -> Move:
@@ -37,16 +39,18 @@ def random_move(ctx: GameContext) -> Move:
     move_weights = [1.0 for _ in legal_moves]  # Uniform weights for random choice
     return random.choices(legal_moves, weights=move_weights, k=1)[0]
 
+
 def get_move_from_mcts(ctx: GameContext) -> Move:
     print("Making MCTS move!")
 
     ponder_time = min(int(1e9), ctx.timeLeft // 10 * 1e6)
     print("Ponder time (ns):", ponder_time)
 
-    move = mcts.ponder_time(board=ctx.board, time_ns=time.time(), simulation_time_ns=ponder_time)
+    move = mcts.ponder_time(board=ctx.board, time_ns=ponder_time)
     print("MCTS selected move:", move)
 
     return move
+
 
 @chess_manager.entrypoint
 def get_move(ctx: GameContext) -> Move:
@@ -57,6 +61,7 @@ def get_move(ctx: GameContext) -> Move:
     except Exception as e:
         print("ERROR OCCURRED:", str(e))
     return move
+
 
 @chess_manager.reset
 def reset_func(ctx: GameContext):
